1135c77946eb1ab2b8270190a996ff43705df889
Tupis <joaoh.tupinamba@gmail.com>
2022-09-08 16:41:57 -0300

    feat: arquivo separado pros botões

---
+++ src/buttons.js
@@ -0,0 +1,35 @@
+import Rock from './assets/icon-rock.svg';
+import Paper from './assets/icon-paper.svg';
+import Scissors from './assets/icon-scissors.svg';
+import Lizard from './assets/icon-lizard.svg';
+import Spock from './assets/icon-spock.svg';
+
+const buttons = {
+    rock: {
+        name: 'Rock',
+        icon: Rock,
+        color: 'hsl(349, 71%, 52%)'
+    },
+    paper: {
+        name: 'Paper',
+        icon: Paper,
+        color: 'hsl(230, 89%, 62%)'
+    },
+    scissors: {
+        name: 'Scissors',
+        icon: Scissors,
+        color: 'hsl(39, 89%, 49%)'
+    },
+    lizard: {
+        name: 'Lizard',
+        icon: Lizard,
+        color: 'hsl(261, 73%, 60%)'
+    },
+    spock: {
+        name: 'Spock',
+        icon: Spock,
+        color: 'hsl(189, 59%, 53%)'
+    }
+}
+
+export default buttons;
