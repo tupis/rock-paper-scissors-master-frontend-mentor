53ae8e65679da003799ba64cd6afce77008e0197
Tupis <joaoh.tupinamba@gmail.com>
2022-09-08 16:45:23 -0300

    feat: exibição do resultado real

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,24 +1,44 @@
 import ButtonGame from "./ButtonGame";
+import '../sass/result.scss';
+import buttons from '../buttons';
 
-const Result = ({setResult}) => {
+const Result = ({setScreenResult, plays, result}) => {
+
+    const player = Object.values(buttons).find(item => item.name === plays.player)
+    const computer = Object.values(buttons).find(item => item.name === plays.computer)
+
     return (
         <>  
             <div className="result">
-                <div className="playerArea">
+                <div className="player-area">
+                    <p>You picked</p>
                     <ButtonGame 
-
+                        icon={player.icon} 
+                        borderColor={player.color}
                     />
                 </div>
 
-                <button 
-                    onClick={() => setResult(false)}
-                >   
-                    Voltar a tela principal
-                </button>
+                <div className="info-result">
+                    <span>
+                        {result === 'draw' 
+                            ? 'Draw'
+                            :  result === 'win'
+                                ? 'You win'
+                                :  'You lose'
+                        }
+                    </span>
+                    <button
+                        onClick={() => setScreenResult(false)}
+                    >   
+                        Play Again
+                    </button>
+                </div>
 
-                <div className="computerArea">
+                <div className="computer-area">
+                    <p>The house picked</p>
                     <ButtonGame 
-
+                        icon={computer.icon} 
+                        borderColor={computer.color}
                     />
                 </div>
             </div>
